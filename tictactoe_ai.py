7a8e80b0c57fa764a63f622c0e7e31ce6b4a4c2f
CanadaUmi <35676085+umi2004@users.noreply.github.com>
2023-05-19 22:40:33 -0400

    Add couple features in board function. Prepare for writing AI.

--- tictactoe_ai.py
+++ tictactoe_ai.py
@@ -14,13 +14,62 @@ scn.fill(BG_C)  # Background Colour
 class Board:
     def __init__(self):
         self.squares = np.zeros((RS, CS))
+        self.empty_sqrs = self.squares  # list of squares
+        self.marked_sqrs = 0  # list of numbers
+
+    def final_state(self):
+        """
+            @return 0 if there is no win yet
+            @return 1 if player 1 wins
+            @return 2 if player 2 wins
+        """
+
+        # vertical wins
+        for col in range(CS):
+            if self.squares[0][col] == self.squares[1][col] == \
+                    self.squares[2][col] != 0:
+                return self.squares[0][col]  # call for player number
+
+        # horizontal wins
+        for row in range(RS):
+            if self.squares[row][0] == self.squares[row][1] == \
+                    self.squares[row][2] != 0:
+                return self.squares[row][0]  # call for player number
+
+        # desc diagonal
+        if self.squares[0][0] == self.squares[1][1] == \
+                self.squares[2][2] != 0:
+            return self.squares[1][1]  # call for player number
+
+        # asc diagonal
+        if self.squares[2][0] == self.squares[1][1] == \
+                self.squares[0][2] != 0:
+            return self.squares[1][1]  # call for player number
+
+        return 0  # still there's no win yet
 
     def mark_sqr(self, row, col, player):
         self.squares[row][col] = player
+        self.marked_sqrs += 1
 
     def empty_sqr(self, row, col):
         return self.squares[row][col] == 0
 
+    # Get the current empty squares left in the game
+    def get_empty_sqrs(self):
+        empty_sqrs = []
+        for row in range(RS):
+            for col in range(CS):
+                if self.empty_sqr(row,col):
+                    empty_sqrs.append((row,col))
+        return empty_sqrs
+
+    # Check if the marked squares become full
+    def isfull(self):
+        return self.marked_sqrs == 9
+
+    def isempty(self):
+        return self.marked_sqrs == 0
 
 # Game Class
 class Game:
